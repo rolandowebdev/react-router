709bd0f569571ff88e088c0b8cc910caa323dd9b
RolandoPranataAR <rolandopranata@gmail.com>
2022-09-27 22:58:15 +0700

    learn how to handle 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { Navbar } from './components';
 import { About, Home, OrderSummary, Layout } from './pages';
+import { NotFound } from './pages/NotFound';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='order-summary' element={<OrderSummary />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </Layout>
   );
