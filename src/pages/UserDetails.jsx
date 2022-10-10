0f8f4a375a3d5bfc29313ee94d3ce1a2fccd2086
RolandoPranataAR <rolandopranata@gmail.com>
2022-10-10 20:35:41 +0700

    learn URL params

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,3 +1,6 @@
+import { useParams } from 'react-router-dom';
+
 export const UserDetails = () => {
-  return <h1 className='mt-2 text-xl'>Detail about user</h1>;
+  const { userId } = useParams();
+  return <h1 className='mt-2 text-xl'>Detail about user {userId}</h1>;
 };
