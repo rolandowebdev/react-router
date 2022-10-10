edf38eae771787ad105cdf8f21abc625aca6e667
RolandoPranataAR <rolandopranata@gmail.com>
2022-10-10 20:48:26 +0700

    learn search params

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -2,5 +2,5 @@ import { useParams } from 'react-router-dom';
 
 export const UserDetails = () => {
   const { userId } = useParams();
-  return <h1 className='mt-2 text-xl'>Detail about user {userId}</h1>;
+  return <h1 className='mt-4 text-xl'>Detail about user {userId}</h1>;
 };
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,12 +1,31 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useSearchParams } from 'react-router-dom';
 
 export const Users = () => {
+  const [searchParams, serSearchParams] = useSearchParams();
+  const showActiveUsers = searchParams.get('filter') === 'active';
   return (
     <div>
       <h1>User 1</h1>
       <h2>User 2</h2>
       <h3>User 3</h3>
       <Outlet />
+      <div className='flex gap-4 mt-4 text-slate-800 text-sm'>
+        <button
+          onClick={() => serSearchParams({ filter: 'active' })}
+          className='bg-slate-400 px-4 rounded-sm py-2'>
+          Active Users
+        </button>
+        <button
+          onClick={() => serSearchParams({})}
+          className='bg-slate-400 px-4 rounded-sm py-2'>
+          Reset Filter
+        </button>
+      </div>
+      {showActiveUsers ? (
+        <h2>Showing active users</h2>
+      ) : (
+        <h2>Showing all users</h2>
+      )}
     </div>
   );
 };
